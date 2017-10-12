fe08e9f53a3056091d9973e1efdf5104358aef83
Philip Schlump <pschlump@gmail.com>
2017-10-12 12:10:50 -0600

    Synchronizing Updates

--- test001.sh
+++ test001.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo test001 - test with wget
+echo test001 - test with wget - send email to pschlump@gmail.com
 
 wget --no-check-certificate -o test001.o1 -O test001.o2 'https://52.21.71.211/api/send?to=pschlup@gmail.com&from=pschlump@gmail.com&bodytext=hi001&bodhhtml=hi001&auth_token=Dg9Tp4ecr8Y3H19lQZtGwFX3ug&subject=new'
 
